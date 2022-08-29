677043be4b079bc1ffbea1a9c96c44b382773754
Sreggie <darren.rogers06@gmail.com>
2022-08-29 19:04:36 -0400

    created boxes for current weather and 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,43 @@
+var searchEl = document.querySelector('#searchCity')
 var appid = '692efab00ae66e9f48137e6ea4766fcd'
-var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${appid}`;
+//var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${appid}`;
 
 var toJSON = function(response){
     return response.json();
 }
 var displayWeather = function(data){
+    console.log(data)
+    var currentWeatherEl = document.querySelector('#currentWeather')
     var h2El = document.createElement('h2')
     var pEl = document.createElement('p')
-    h2El.textContent = city.name
+    h2El.textContent = data.name
     pEl.textContent = 'Temp: ' + data.current.temp
-    document.body.appendChild(h2El)
-    document.body.appendChild(pEl)
+    currentWeatherEl.appendChild(h2El)
+    currentWeatherEl.appendChild(pEl)
 
 }
-var getOneCallc = function(city){
-    var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${city.lat}&lon=${city.lon}&appid=${appid}&//units=imperial&exclude=hourly,minutely`
+var getOneCall = function(city){
+    var oneCall = `https://api.openweathermap.org/data/3.0/onecall?lat=${city.lat}&lon=${city.lon}&appid=${appid}&//units=imperial&exclude=hourly,minutely`
 
     fetch(oneCall)
-        .then(toJSON)
-        .then(displayWeather)
+    .then(toJSON)
+    .then(displayWeather)
 }
 var getGeo = function(locations){
     var city = locations[0]
     console.log('LAT', city.lat)
     console.log('LON', city.lon)
+
+    getOneCall(city)
 }
+var handler = function(event){
+    event.preventDefault
+    var q = document.querySelector('#input').value
+    var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${appid}`;
+    fetch(geoURL)
+    .then(toJSON)
+    .then(getGeo)
+
+}
+
+searchEl.addEventListener('click', handler)
