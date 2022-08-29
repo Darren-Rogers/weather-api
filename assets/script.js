0f06a17c9fea91a1421c53548fcffed3a5f11310
Sreggie <darren.rogers06@gmail.com>
2022-08-29 19:47:56 -0400

    added daily temp, uvi, wind and humidity

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
 var searchEl = document.querySelector('#searchCity')
 var appid = '692efab00ae66e9f48137e6ea4766fcd'
-//var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${appid}`;
 
 var toJSON = function(response){
     return response.json();
@@ -10,10 +9,19 @@ var displayWeather = function(data, city){
     var currentWeatherEl = document.querySelector('#currentWeather')
     var h2El = document.createElement('h2')
     var pEl = document.createElement('p')
+    var humidityEl = document.createElement('p')
+    var windEl = document.createElement('p')
+    var uviEl = document.createElement('p')
     h2El.textContent = city.name
-    pEl.textContent = 'Temp: ' + data.current.temp
+    pEl.textContent = 'Temperature: ' + data.current.temp
+    humidityEl.textContent = 'Humidity: '+data.daily[0].humidity
+    windEl.textContent = 'Wind Speed: '+data.daily[0].wind_speed
+    uviEl.textContent = 'UV Index: '+data.daily[0].uvi
     currentWeatherEl.appendChild(h2El)
     currentWeatherEl.appendChild(pEl)
+    currentWeatherEl.appendChild(humidityEl)
+    currentWeatherEl.appendChild(windEl)
+    currentWeatherEl.appendChild(uviEl)
 
 }
 var getOneCall = function(city){
@@ -25,19 +33,19 @@ var getOneCall = function(city){
             displayWeather(data, city);
         })
 }
-var getGeo = function(locations){
-    var city = locations[0]
-    console.log('LAT', city.lat);
-    console.log('LON', city.lon);
-
-    getOneCall(city)
-}
-var localStorage = function(city){
+var toLocalStorage = function(city){
     var citySaved = JSON.parse(localStorage.getItem('cities')) || [];
     citySaved.push(city)
     var saved = JSON.stringify(citySaved);
     localStorage.setItem('cities', saved)
 
+}
+var getGeo = function(locations){
+    var city = locations[0]
+    console.log('LAT', city.lat);
+    console.log('LON', city.lon);
+    toLocalStorage(city.name)
+    getOneCall(city)
 }
 var handler = function(event){
     event.preventDefault()
@@ -48,5 +56,4 @@ var handler = function(event){
         .then(getGeo)
 
 }
-
 searchEl.addEventListener('click', handler)
